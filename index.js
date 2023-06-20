c746a24efb048ddcee83db5313760809af5f3a3b
Jose <jsut914@gmail.com>
2023-06-20 11:15:29 -0600

    Finalizando proyecto

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+function tiempoReloj(){
 var fechaActual = new Date(); 
 var horas = fechaActual.getHours();
 var minutos = fechaActual.getMinutes();
 var segundos = fechaActual.getSeconds();
 
-console.log(fechaActual); 
-console.log(horas); 
-console.log(minutos); 
-console.log(segundos); 
-
 var conexion = document.querySelector('#reloj');
 var horaReloj = horas +" : "+ minutos+" : " + segundos;
 
-conexion.innerHTML = horaReloj;
+conexion.innerHTML = horaReloj;}
+
+setInterval(tiempoReloj, 1000);
